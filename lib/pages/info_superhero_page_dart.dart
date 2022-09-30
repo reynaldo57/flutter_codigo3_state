c9fe5c49836e1223d1d6acf8f02e1aa6e7f8e2bc
reynaldo57 <reynaldo57@hotmail.com>
2022-09-30 12:26:02 -0500

    provider4 (segunda forma)

--- lib/pages/info_superhero_page_dart.dart
+++ lib/pages/info_superhero_page_dart.dart
@@ -1,6 +1,5 @@
-
-
 import 'package:flutter/material.dart';
+import 'package:flutter_codigo_3_state/model/super_hero_model.dart';
 import 'package:flutter_codigo_3_state/pages/register_superhero_page.dart';
 import 'package:flutter_codigo_3_state/services/superheroe_services.dart';
 import 'package:provider/provider.dart';
@@ -10,58 +9,68 @@ class InfoSuperheroPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     final superheroe = Provider.of<SuperheroService>(context);
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text("SuperHeroe"),
-        backgroundColor: Colors.deepPurpleAccent,
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){
-          Navigator.push(context, MaterialPageRoute(builder: (context)=>RegisterSuperHeroPage()));
-        },
-        backgroundColor: Colors.deepPurpleAccent,
-        child: Icon(Icons.add),
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 16, vertical: 20.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                "Informacion General",
-                style: TextStyle(
-                  fontSize: 24.0,
-                  fontWeight: FontWeight.bold
-                ),
-              ),
-              Divider(),
-              ListTile(
-                title: Text("Nombre: ${superheroe.superheroe.name}"),
-              ),
-              ListTile(
-                title: Text("Años de Experiencia: ${superheroe.superheroe.experience}"),
-              ),
-              Text(
-                "Poderes",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20.0
-                ),
-              ),
-              Divider(),
+        appBar: AppBar(
+          title: Text("SuperHeroe"),
+          backgroundColor: Colors.deepPurpleAccent,
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => RegisterSuperHeroPage()));
+          },
+          backgroundColor: Colors.deepPurpleAccent,
+          child: Icon(Icons.add),
+        ),
+        body: superheroe.superheroeExist
+            ? SupeheroeInformationWidget(
+                superheroe: superheroe.superheroe,
+              )
+            : Center(
+                child: Text("Aun no hay superheroe"),
+              ));
+  }
+}
 
-              ...superheroe.superheroe.powers.map(
-                  (e) => ListTile(
-                    title: Text(e),
-                  ),
-              ),
+class SupeheroeInformationWidget extends StatelessWidget {
+  Superheroe superheroe;
 
-            ],
-          ),
+  SupeheroeInformationWidget({required this.superheroe});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16, vertical: 20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Informacion General",
+              style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+            ),
+            Divider(),
+            ListTile(
+              title: Text("Nombre: ${superheroe.name}"),
+            ),
+            ListTile(
+              title: Text("Años de Experiencia: ${superheroe.experience}"),
+            ),
+            Text(
+              "Poderes",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+            ),
+            Divider(),
+            ...superheroe.powers.map(
+              (e) => ListTile(
+                title: Text(e),
+              ),
+            ),
+          ],
         ),
       ),
     );
