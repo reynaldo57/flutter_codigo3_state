65a0e2effd733dfc07d45f32e0b3c9ab06782d56
reynaldo57 <reynaldo57@hotmail.com>
2022-10-03 11:15:33 -0500

    cubit 2

--- lib/pages/info_superhero_page_dart.dart
+++ lib/pages/info_superhero_page_dart.dart
@@ -1,5 +1,8 @@
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_codigo_3_state/cubit_superheroe/superheroe_cubit.dart';
+import 'package:flutter_codigo_3_state/model/super_hero_model.dart';
 import 'package:flutter_codigo_3_state/pages/register_superhero_page.dart';
 
 class InfoSuperheroPage extends StatelessWidget {
@@ -19,50 +22,75 @@ class InfoSuperheroPage extends StatelessWidget {
         backgroundColor: Colors.deepPurpleAccent,
         child: Icon(Icons.add),
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 16, vertical: 20.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                "Informacion General",
-                style: TextStyle(
-                    fontSize: 24.0,
-                    fontWeight: FontWeight.bold
-                ),
-              ),
-              Divider(),
-              ListTile(
-                title: Text("Nombre: "),
-              ),
-              ListTile(
-                title: Text("Años de Experiencia: "),
-              ),
-              Text(
-                "Poderes",
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20.0
-                ),
-              ),
-              Divider(),
+      body: BlocBuilder<SuperheroeCubit, SuperheroeState>(
+        builder: (context, state) {
+          switch (state.runtimeType){
+            case SuperheroeInit:
+              return Center(child: Text("Aun no hay superheroe"),);
+            case SuperheroeCreate:
+              return InfoSuperheroeWidget(superheroe: (state as SuperheroeCreate).superheroe,);
+            default:
+              return Center(child: CircularProgressIndicator(),);
+          }
+        },
+      ),
+    );
+  }
+}
 
-              ListTile(
-                title: Text("Poder 1: "),
-              ),
-              ListTile(
-                title: Text("Poder 2: "),
-              ),
-              ListTile(
-                title: Text("Poder 3: "),
-              ),
-              ListTile(
-                title: Text("Poder 4: "),
-              ),
+class InfoSuperheroeWidget extends StatelessWidget {
 
-            ],
-          ),
+  final Superheroe superheroe;
+
+  InfoSuperheroeWidget({
+    required this.superheroe
+});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16, vertical: 20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Informacion General",
+              style: TextStyle(
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold
+              ),
+            ),
+            Divider(),
+            ListTile(
+              title: Text("Nombre: ${superheroe.name}"),
+            ),
+            ListTile(
+              title: Text("Años de Experiencia: ${superheroe.experience}"),
+            ),
+            Text(
+              "Poderes",
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20.0
+              ),
+            ),
+            Divider(),
+
+            ListTile(
+              title: Text("Poder 1: "),
+            ),
+            ListTile(
+              title: Text("Poder 2: "),
+            ),
+            ListTile(
+              title: Text("Poder 3: "),
+            ),
+            ListTile(
+              title: Text("Poder 4: "),
+            ),
+
+          ],
         ),
       ),
     );
