241b1a74ec1b795dfd51dccd74b6d4775557d2d6
reynaldo57 <reynaldo57@hotmail.com>
2022-10-05 18:25:56 -0500

    Superheroe bloc 4

--- lib/pages/info_superhero_page_dart.dart
+++ lib/pages/info_superhero_page_dart.dart
@@ -1,6 +1,8 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_codigo_3_state/bloc/superheroe/superheroe_bloc.dart';
 import 'package:flutter_codigo_3_state/pages/register_superhero_page.dart';
 
 class InfoSuperheroPage extends StatelessWidget {
@@ -20,50 +22,67 @@ class InfoSuperheroPage extends StatelessWidget {
         backgroundColor: Colors.deepPurpleAccent,
         child: Icon(Icons.add),
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 16, vertical: 20.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                "Informacion General",
-                style: TextStyle(
-                  fontSize: 24.0,
-                  fontWeight: FontWeight.bold
-                ),
-              ),
-              Divider(),
-              ListTile(
-                title: Text("Nombre: "),
-              ),
-              ListTile(
-                title: Text("Años de Experiencia: "),
-              ),
-              Text(
-                "Poderes",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20.0
-                ),
-              ),
-              Divider(),
+      body: BlocBuilder<SuperheroeBloc, SuperheroeState>(
+        builder: (context, state){
+          if(state.superheroeExist){
+            return InfoSuperheroeWidget();
+          }else{
+            return Center(child: Text("Aun no hayy Superheroe"),);
+          }
 
-              ListTile(
-                title: Text("Poder 1: "),
-              ),
-              ListTile(
-                title: Text("Poder 2: "),
-              ),
-              ListTile(
-                title: Text("Poder 3: "),
-              ),
-              ListTile(
-                title: Text("Poder 4: "),
-              ),
+        },
+      )
+    );
+  }
+}
 
-            ],
-          ),
+class InfoSuperheroeWidget extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16, vertical: 20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Informacion General",
+              style: TextStyle(
+                fontSize: 24.0,
+                fontWeight: FontWeight.bold
+              ),
+            ),
+            Divider(),
+            ListTile(
+              title: Text("Nombre: "),
+            ),
+            ListTile(
+              title: Text("Años de Experiencia: "),
+            ),
+            Text(
+              "Poderes",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20.0
+              ),
+            ),
+            Divider(),
+
+            ListTile(
+              title: Text("Poder 1: "),
+            ),
+            ListTile(
+              title: Text("Poder 2: "),
+            ),
+            ListTile(
+              title: Text("Poder 3: "),
+            ),
+            ListTile(
+              title: Text("Poder 4: "),
+            ),
+
+          ],
         ),
       ),
     );
